9ce7aba0cf7d1e8fc8d839de4973183beab77ae7
Ori Lindner <olindner@umich.edu>
2020-08-19 14:33:04 -0400

    Added XR Interation package. Tried to make fire blast, materials and particles are causing problems. Likely switch to earth bending work.

---
+++ Assets/Scripts/BendingController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class BendingController : MonoBehaviour
+{
+    private XRGrabInteractable grabbable; 
+    private List<InputDevice> rightHandDevice = new List<InputDevice>();
+    private ParticleSystem particleSystem;
+
+    void Start()
+    {
+        particleSystem = GetComponent<ParticleSystem>();
+        particleSystem.Stop();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, rightHandDevice);
+    }
+
+    void Update()
+    {
+        bool triggerValue;
+        if(rightHandDevice.Count > 0 && rightHandDevice[0].TryGetFeatureValue(CommonUsages.triggerButton, out triggerValue) && triggerValue)
+        {
+            ShootFire();
+        }
+        else
+        {
+            particleSystem.Stop();
+        }
+    }
+
+    public void ShootFire()
+    {
+        particleSystem.Play();
+    }
+}
